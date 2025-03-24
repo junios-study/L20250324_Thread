1a9e7bacc262c0343b6a960e21f70a8961a760bb
junios-study <junios@naver.com>
2025-03-24 12:45:02 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L20250324_Thread
+{
+    class Program
+    {
+        static int Money = 0;
+
+        static void Add()
+        {
+            for(int i = 0; i < 1000000; ++i)
+            {
+                Money++;
+            }
+        }
+
+        static void Remove()
+        {
+            for (int i = 0; i < 1000000; ++i)
+            {
+                Money--;
+            }
+        }
+
+        //foreground, main thread 종료 되면 나머지 쓰레드는 다 종료
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(Add));
+            Thread thread2 = new Thread(new ThreadStart(Remove));
+
+            //B함수 따로 실행 시켜줘 (Thread) -> OS 부탁
+            thread1.IsBackground = true;
+            thread1.Start();
+            thread2.IsBackground = true;
+            thread2.Start();
+
+
+            thread1.Join();
+            thread2.Join();
+
+
+            Console.WriteLine(Money);
+        }
+    }
+}
